3918d7c072036789cfcb8eafb7c92e579c240347
reshinto <terencekong2002@gmail.com>
2019-02-06 21:13:01 +0800

    completed app, but lack user error prevention

--- h1b.py
+++ h1b.py
@@ -5,37 +5,86 @@ __version__ 0.1
 import datetime
 import requests
 import re
+import statistics as stat
 
 
-def default_year():
-    """
-    Set the default year to the previous year,
-    because data for the current year is most probably unavailable.
-    """
-    return datetime.datetime.now().year - 1
-
-
-class h1b:
+class H1b_database:
     """
 
     """
-    def __init__(self, company, year=None):
-        self.company = company.lower()
-        self.year = default_year() if year is None else year
-        self.url = f"https://h1bdata.info/index.php?em={self.company}" +\
-                   f"&year={self.year}"
+    def __init__(self, company, job_title, year=None):
+        # ensures company name is all in lowercase and capitalized for display
+        self.company = company.lower().capitalize()
+        # ensures each word is capitalized for display
+        self.title = job_title.title()
+        self.year = self.default_year() if year is None else year
+        # lower() is used to ensure company name is in lowercase during search
+        self.url = "https://h1bdata.info/index.php?em=" +\
+                   f"{self.company.lower()}&year={self.year}"
+        # calculate one time and store value for easy reference
+        self.salaries = self.get_salaries()
+
+    @staticmethod
+    def default_year():
+        """
+        This will set the default year to the previous year,
+        because data for the current year is most probably unavailable
+        """
+        return datetime.datetime.now().year - 1
 
     def raw_html(self):
         """This is return a string of the entire raw html data"""
-        return requests.get("{}".format(self.url)).text
+        return requests.get(f"{self.url}").text
 
-    def search_title(self, title):
-        search = re.compile(title.upper())
-        matches = search.finditer(self.raw_html())
-        for match in matches:
-            print(match)
+    def get_salaries(self):
+        salary_list = []
+        search = re.compile(rf'{self.title.upper()}\s\(?[0-9]+.[0-9]+\)?' +
+                            '.....................................' +
+                            '.....................................' +
+                            '........................[0-9]+,[0-9]+')
+        matches = search.finditer(self.raw_html())  # get search results
+        if matches is not None:
+            for match in matches:
+                string = match.group(0)  # extract string from search result
+                index = string.find("$")  # find salary that starts with $ sign
+                if index != -1:
+                    # extract salary and remove the , sign
+                    salary = int(string[index+1:].replace(",", ""))
+                    salary_list.append(salary)
+                else:
+                    return None
+            salary_list.sort()
+        else:
+            salary_list = None
+        return salary_list
+
+    def min_salary(self):
+        """Find the lowest salary"""
+        return min(self.salaries)
+
+    def max_salary(self):
+        """Find the highest salary"""
+        return max(self.salaries)
+
+    def avg_salary(self):
+        """Find average salary"""
+        return round(stat.mean(self.salaries))
+
+    def median_salary(self):
+        """Find the median salary"""
+        return round(stat.median(self.salaries))
+
+    def salary_stdev(self):
+        """Find the salary standard deviation"""
+        return round(stat.stdev(self.salaries))
+
+    def salary_mode(self):
+        """Find the most common given salary"""
+        try:
+            return stat.mode(self.salaries)
+        except stat.StatisticsError:
+            return "unavailable because multiple modes exists"
 
 
 if __name__ == "__main__":
-    test = h1b("Google")
-    test.search_title("software engineer")
+    pass
---
+++ menu.py
@@ -0,0 +1,36 @@
+from h1b import H1b_database
+
+
+def addComma(integer):
+    """
+    Add a comma after every 3 integers from right to left
+    """
+    strList = list(str(integer))
+    if len(strList) > 3:
+        for i in range(len(strList)-3, -1, -3):
+            strList.insert(i, ",")
+    if strList[0] == ",":
+        strList.pop(0)
+    finalStr = "".join(strList)
+    return finalStr
+
+
+def menu():
+    company = input("Company name: ")
+    job_title = input("Job Title name: ")
+    default_year = input("Set year as most recent [y/n]? ").lower()
+    year = int(input("Year: ")) if default_year == "n" else None
+    # year = None if default_year == "y" else int(input("Year: "))
+    data = H1b_database(company, job_title, year)
+    # let x be the average salary to test conditions
+    x = data.salary_mode()
+    avgSalary = "$" + addComma(x) if type(x) == int else data.salary_mode()
+    print(f"\nThe annual salary for {data.title}s at {data.company}"
+          f" in the year {data.year}:"
+          f"\n - Min is ${addComma(data.min_salary())}, "
+          f"Average is ${addComma(data.avg_salary())},"
+          f" Max is ${addComma(data.max_salary())}")
+    print(f" - Average salary is ${addComma(data.median_salary())}")
+    print(" - The most common salary given is "
+          f"{avgSalary}")
+    print(f" - The standard deviation is {data.salary_stdev()}")
